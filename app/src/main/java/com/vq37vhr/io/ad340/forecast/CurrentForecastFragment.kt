608926ad1d0050e0d0f773c20a2948b2868256fe
vq37vhr <sunnylau1206@gmail.com>
2020-05-20 01:08:24 -0700

    assignment4

---
+++ app/src/main/java/com/vq37vhr/io/ad340/forecast/CurrentForecastFragment.kt
@@ -0,0 +1,91 @@
+package com.vq37vhr.io.ad340.forecast
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.vq37vhr.io.ad340.*
+
+import com.vq37vhr.io.ad340.details.ForecastDetailsActivity
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CurrentForecastFragment : Fragment() {
+
+    private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
+    private val forecastRepository = ForecastRepository()
+
+    private lateinit var appNavigator: AppNavigator
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        appNavigator = context as AppNavigator
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        tempDisplaySettingManager = TempDisplaySettingManager(requireContext())
+
+        val zipcode = arguments!!.getString(KEY_ZIPCODE) ?: ""
+
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_current_forecast, container, false)
+
+        val locationEntryButton: FloatingActionButton = view.findViewById(R.id.locationEntryButton)
+        locationEntryButton.setOnClickListener {
+            appNavigator.navigatorToLocationEntry()
+        }
+
+        val dailyForecastList: RecyclerView = view.findViewById(R.id.dailyForecastList)
+        dailyForecastList.layoutManager = LinearLayoutManager(requireContext())
+        val dailyForecastAdapter = DailyForecastAdapter(tempDisplaySettingManager) {
+            showForecastDetails(it)
+        }
+
+        dailyForecastList.adapter = dailyForecastAdapter
+
+        // Create the observer which updates the UI in response to forecast updates
+        val weeklyForecastObserver = Observer<List<DailyForecast>> { forecastItems ->
+            // update our list adapter
+            dailyForecastAdapter.submitList(forecastItems)
+        }
+        forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
+
+        forecastRepository.loadForecast(zipcode)
+
+        return view
+    }
+
+
+    private fun showForecastDetails(forecast: DailyForecast){
+        val forecastDetailsIntent = Intent(requireContext(), ForecastDetailsActivity::class.java)
+        forecastDetailsIntent.putExtra("key_temp", forecast.temp)
+        forecastDetailsIntent.putExtra("key_description", forecast.description)
+        startActivity(forecastDetailsIntent)
+    }
+
+    companion object {
+        const val KEY_ZIPCODE = "key_zipcode"
+
+        fun newInstance(zipcode: String) : CurrentForecastFragment {
+            val fragment = CurrentForecastFragment()
+
+            val args = Bundle()
+            args.putString(KEY_ZIPCODE, zipcode)
+            fragment.arguments = args
+
+            return fragment
+        }
+    }
+
+}
